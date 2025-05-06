4eecbd01f34329079afbf1ad36172984e8085c12
Fahri-Hosting <147958381+GZSTORE@users.noreply.github.com>
2025-05-06 15:45:47 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Efek scroll dan transisi lainnya (AOS Init)
+document.addEventListener("DOMContentLoaded", () => {
+  if (AOS) {
+    AOS.init();
+  }
+
+  console.log("NexTarSMP Website Loaded");
+});
